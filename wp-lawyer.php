466c6ffce3423a6f43fc56b92e5b1bb34778d293
Brandon Hubbard <bkhubbard@gmail.com>
2014-05-19 14:16:25 -0700

    Added Attorney Custom Post Type

---
+++ wp-lawyer.php
@@ -0,0 +1,66 @@
+<?php
+
+/*!-----------------------------------------------------------------------------------
+
+  Plugin Name: WordPress SEO
+  Version: 1.0.0
+  Plugin URI: https://github.com/bhubbard/wp-lawyer
+  Description: A custom WordPress plugin for lawyers and Law Firms.
+  Author: Brandon Hubbard
+  Author URI: https://wp-lawyer.com/
+  Text Domain: wp-lawyer
+  Domain Path: /languages/
+  License: GPL v3
+
+-----------------------------------------------------------------------------------*/
+
+
+
+if ( ! function_exists('wp_lawyer_attorneys_cpt') ) {
+
+// Register Custom Post Type
+function wp_lawyer_attorneys_cpt() {
+
+	$labels = array(
+		'name'                => _x( 'Attorneys', 'Post Type General Name', 'wp-lawyer' ),
+		'singular_name'       => _x( 'attorney', 'Post Type Singular Name', 'wp-lawyer' ),
+		'menu_name'           => __( 'Attorney', 'wp-lawyer' ),
+		'parent_item_colon'   => __( 'Parent Attorney:', 'wp-lawyer' ),
+		'all_items'           => __( 'All Attorneys', 'wp-lawyer' ),
+		'view_item'           => __( 'View Attorney', 'wp-lawyer' ),
+		'add_new_item'        => __( 'Add New Attorney', 'wp-lawyer' ),
+		'add_new'             => __( 'Add Attorney', 'wp-lawyer' ),
+		'edit_item'           => __( 'Edit Attorney', 'wp-lawyer' ),
+		'update_item'         => __( 'Update Attorney', 'wp-lawyer' ),
+		'search_items'        => __( 'Search Attorneys', 'wp-lawyer' ),
+		'not_found'           => __( 'No Attorney Found', 'wp-lawyer' ),
+		'not_found_in_trash'  => __( 'No Attorney Found in Trash', 'wp-lawyer' ),
+	);
+	$args = array(
+		'label'               => __( 'attorney', 'wp-lawyer' ),
+		'description'         => __( 'Attorney', 'wp-lawyer' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'custom-fields', ),
+		'taxonomies'          => array( 'practice-type' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => '',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'attorney', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'wp_lawyer_attorneys_cpt', 0 );
+
+}
