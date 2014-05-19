5d5c940e33d6d012d859bb3c57e49ab20496b251
Brandon Hubbard <bkhubbard@gmail.com>
2014-05-19 14:20:11 -0700

    Cases CPT

--- wp-lawyer.php
+++ wp-lawyer.php
@@ -15,7 +15,9 @@
 -----------------------------------------------------------------------------------*/
 
 
-
+###########################################################
+// Register Attorneys Custom Post Type
+###########################################################
 if ( ! function_exists('wp_lawyer_attorneys_cpt') ) {
 
 // Register Custom Post Type
@@ -62,5 +64,57 @@ function wp_lawyer_attorneys_cpt() {
 
 // Hook into the 'init' action
 add_action( 'init', 'wp_lawyer_attorneys_cpt', 0 );
+
+}
+
+###########################################################
+// Register Cases Custom Post Type
+###########################################################
+if ( ! function_exists('wp_lawyer_cases_cpt') ) {
+
+// Register Custom Post Type
+function wp_lawyer_cases_cpt() {
+
+	$labels = array(
+		'name'                => _x( 'Cases', 'Post Type General Name', 'wp-lawyer' ),
+		'singular_name'       => _x( 'Case', 'Post Type Singular Name', 'wp-lawyer' ),
+		'menu_name'           => __( 'Cases', 'wp-lawyer' ),
+		'parent_item_colon'   => __( 'Parent Case:', 'wp-lawyer' ),
+		'all_items'           => __( 'All Cases', 'wp-lawyer' ),
+		'view_item'           => __( 'View Case', 'wp-lawyer' ),
+		'add_new_item'        => __( 'Add New Case', 'wp-lawyer' ),
+		'add_new'             => __( 'Add Case', 'wp-lawyer' ),
+		'edit_item'           => __( 'Edit Case', 'wp-lawyer' ),
+		'update_item'         => __( 'Update Case', 'wp-lawyer' ),
+		'search_items'        => __( 'Search Cases', 'wp-lawyer' ),
+		'not_found'           => __( 'No Cases Found', 'wp-lawyer' ),
+		'not_found_in_trash'  => __( 'No Cases Found in Trash', 'wp-lawyer' ),
+	);
+	$args = array(
+		'label'               => __( 'cases', 'wp-lawyer' ),
+		'description'         => __( 'Cases', 'wp-lawyer' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'custom-fields', ),
+		'taxonomies'          => array( 'case-type' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => '',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'cases', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'wp_lawyer_cases_cpt', 0 );
 
 }
