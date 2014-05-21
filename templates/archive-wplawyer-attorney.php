2e4e585ec942aa58c0342fbe1988112829adbbb8
Brandon Hubbard <bkhubbard@gmail.com>
2014-05-21 16:27:57 -0700

    Started out Templates for Attorneys

--- templates/archive-wplawyer-attorney.php
+++ templates/archive-wplawyer-attorney.php
@@ -5,9 +5,6 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 			
-
-
-		
 		
 		<?php if ( has_post_thumbnail()) : ?>
 						<?php the_post_thumbnail('wplawyer-attorney'); ?>
@@ -17,18 +14,26 @@
 		
 		<?php the_content(); ?>
 		
+		<ul>
+		<li><strong>Title: </strong><?php wplawyer_attorney_title(); ?></li>
+		<li><strong>Bar: </strong><?php wplawyer_attorney_bar_number(); ?></li>
+		<li><strong>Address: </strong><?php wplawyer_attorney_address(); ?></li>
+		<li><strong>Email: </strong><?php wplawyer_attorney_email(); ?></li>
+		<li><strong>Phone: </strong><?php wplawyer_attorney_mobile(); ?></li>
+		<li><strong>Fax: </strong><?php wplawyer_attorney_fax(); ?></li>
+		<li><strong>Website: </strong><?php wplawyer_attorney_website(); ?></li>
+		<li><strong>Facebook: </strong><?php wplawyer_attorney_facebook(); ?></li>
+		<li><strong>Twitter: </strong><?php wplawyer_attorney_twitter(); ?></li>
+		<li><strong>LinkedIn: </strong><?php wplawyer_attorney_linkedin(); ?></li>
+		<li><strong>YouTube: </strong><?php wplawyer_attorney_youtube(); ?></li>
 		
-		<?php wplawyer_attorney_title(); ?>
-		<?php wplawyer_attorney_bar_number(); ?>
-		<?php wplawyer_attorney_address(); ?>
-		<?php wplawyer_attorney_email(); ?>
-		<?php wplawyer_attorney_mobile(); ?>
-		<?php wplawyer_attorney_fax(); ?>
-		<?php wplawyer_attorney_website(); ?>
-		<?php wplawyer_attorney_facebook(); ?>
-		<?php wplawyer_attorney_twitter(); ?>
-		<?php wplawyer_attorney_linkedin(); ?>
-		<?php wplawyer_attorney_youtube(); ?>
+		<li><strong>Areas of Practice: <?php echo get_the_term_list( get_the_ID(), 'wplawyer-practice-area', ' ', ', ', '' ); ?></li>
+		<li><strong>Cities of Practice: <?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-city', ' ', ', ', '' ); ?></li>
+		<li><strong>Counties of Practice: <?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-county', ' ', ', ', '' ); ?></li>
+		<li><strong>Districts: </strong><?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-district', ' ', ', ', '' ); ?></li>
+		<li><strong>Law School: </strong><?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-lawschool', ' ', ', ', '' ); ?></li>
+		<li><strong>Undergrad School: </strong><?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-undergrad', ' ', ', ', '' ); ?></li>
+		</ul>
 		
 		
 		<?php endwhile; else: ?>
--- templates/single-wplawyer-attorney.php
+++ templates/single-wplawyer-attorney.php
@@ -24,35 +24,24 @@ dfsdff
 		<ul>
 		<li><strong>Title: </strong><?php wplawyer_attorney_title(); ?></li>
 		<li><strong>Bar: </strong><?php wplawyer_attorney_bar_number(); ?></li>
-		<li><?php wplawyer_attorney_address(); ?></li>
-		<li><?php wplawyer_attorney_email(); ?></li>
-		<li><?php wplawyer_attorney_mobile(); ?></li>
-		<li><?php wplawyer_attorney_fax(); ?></li>
-		<li><?php wplawyer_attorney_website(); ?></li>
-		<li><?php wplawyer_attorney_facebook(); ?></li>
-		<li><?php wplawyer_attorney_twitter(); ?></li>
-		<li><?php wplawyer_attorney_linkedin(); ?></li>
-		<li><?php wplawyer_attorney_youtube(); ?></li>
+		<li><strong>Address: </strong><?php wplawyer_attorney_address(); ?></li>
+		<li><strong>Email: </strong><?php wplawyer_attorney_email(); ?></li>
+		<li><strong>Phone: </strong><?php wplawyer_attorney_mobile(); ?></li>
+		<li><strong>Fax: </strong><?php wplawyer_attorney_fax(); ?></li>
+		<li><strong>Website: </strong><?php wplawyer_attorney_website(); ?></li>
+		<li><strong>Facebook: </strong><?php wplawyer_attorney_facebook(); ?></li>
+		<li><strong>Twitter: </strong><?php wplawyer_attorney_twitter(); ?></li>
+		<li><strong>LinkedIn: </strong><?php wplawyer_attorney_linkedin(); ?></li>
+		<li><strong>YouTube: </strong><?php wplawyer_attorney_youtube(); ?></li>
+		
+		<li><strong>Areas of Practice: <?php echo get_the_term_list( get_the_ID(), 'wplawyer-practice-area', ' ', ', ', '' ); ?></li>
+		<li><strong>Cities of Practice: <?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-city', ' ', ', ', '' ); ?></li>
+		<li><strong>Counties of Practice: <?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-county', ' ', ', ', '' ); ?></li>
+		<li><strong>Districts: </strong><?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-district', ' ', ', ', '' ); ?></li>
+		<li><strong>Law School: </strong><?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-lawschool', ' ', ', ', '' ); ?></li>
+		<li><strong>Undergrad School: </strong><?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-undergrad', ' ', ', ', '' ); ?></li>
 		</ul>
 		
-		
-		<h3>Areas of Practice</h3>
-		<?php echo get_the_term_list( get_the_ID(), 'wplawyer-practice-area', ' ', ', ', '' ); ?>
-		
-		<h3>Cities of Practice</h3>
-		<?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-city', ' ', ', ', '' ); ?>
-		
-		<h3>Counties of Practice</h3>
-		<?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-county', ' ', ', ', '' ); ?>
-		
-		<h3>Districts</h3>
-		<?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-district', ' ', ', ', '' ); ?>
-		
-		<h3>Law School</h3>
-		<?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-lawschool', ' ', ', ', '' ); ?>
-		
-		<h3>Undergrad School</h3>
-		<?php echo get_the_term_list( get_the_ID(), 'wplawyer-attorney-undergrad', ' ', ', ', '' ); ?>
 		
 
 		<?php endwhile; else: ?>
