a4f61a5ba8977e74ca8194f18bacb8cdb8c5cac8
Brandon Hubbard <bkhubbard@gmail.com>
2014-05-21 16:31:38 -0700

    Basic Title for Taxonomy

--- templates/archive-wplawyer-attorney.php
+++ templates/archive-wplawyer-attorney.php
@@ -2,6 +2,12 @@
 
 			<div class="content">
 			
+			
+			
+			<h1>Attorneys for <?php $term = $wp_query->queried_object; echo $term->name; ?> </h1>
+			
+			
+			
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 			
