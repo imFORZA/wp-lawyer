7c8944ebf9899f4efb2b7cc9022cc9f9926b3c53
Brandon Hubbard <bkhubbard@gmail.com>
2014-05-21 14:52:41 -0700

    Was working in wrong template, oops!

--- templates/archive-wplawyer-attorney.php
+++ templates/archive-wplawyer-attorney.php
@@ -1,5 +1,43 @@
 <?php get_header(); ?>
 
+			<div class="content">
+			
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+			
+
+
+		
+		
+		<?php if ( has_post_thumbnail()) : ?>
+						<?php the_post_thumbnail('wplawyer-attorney'); ?>
+					<?php else : endif; ?>
+						
+		
+		
+		<?php the_content(); ?>
+		
+		
+		<?php wplawyer_attorney_title(); ?>
+		<?php wplawyer_attorney_bar_number(); ?>
+		<?php wplawyer_attorney_address(); ?>
+		<?php wplawyer_attorney_email(); ?>
+		<?php wplawyer_attorney_mobile(); ?>
+		<?php wplawyer_attorney_fax(); ?>
+		<?php wplawyer_attorney_website(); ?>
+		<?php wplawyer_attorney_facebook(); ?>
+		<?php wplawyer_attorney_twitter(); ?>
+		<?php wplawyer_attorney_linkedin(); ?>
+		<?php wplawyer_attorney_youtube(); ?>
+		
+		
+		<?php endwhile; else: ?>
+
+				<p>Sorry, this attorney appears to no longer exist.</p>
+
+		<?php endif; ?>
+
+	</div>
 
 
 <?php get_footer(); ?>
--- templates/single-wplawyer-cases.php
+++ templates/single-wplawyer-cases.php
@@ -1,5 +1,32 @@
 <?php get_header(); ?>
 
+			<div class="content">
+			
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+			
+dfsdff
+
+		
+		
+		<?php if ( has_post_thumbnail()) : ?>
+						<?php the_post_thumbnail('wplawyer-case'); ?>
+					<?php else : endif; ?>
+						
+		
+		
+		<?php the_content(); ?>
+
+
+		
+		
+	<?php endwhile; else: ?>
+
+				<p>Sorry, this case appears to no longer exist.</p>
+
+			<?php endif; ?>
+
+	</div>
 
 
 <?php get_footer(); ?>
